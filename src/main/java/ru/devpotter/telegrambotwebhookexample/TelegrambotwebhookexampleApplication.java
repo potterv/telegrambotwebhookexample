7b15ad582312ca71d0197b220d2f1b711b858792
Владимир Гончар <gonchar@092.pfr.ru>
2022-07-20 17:33:53 +0300

    Added empty project

---
+++ src/main/java/ru/devpotter/telegrambotwebhookexample/TelegrambotwebhookexampleApplication.java
@@ -0,0 +1,13 @@
+package ru.devpotter.telegrambotwebhookexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegrambotwebhookexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegrambotwebhookexampleApplication.class, args);
+	}
+
+}
